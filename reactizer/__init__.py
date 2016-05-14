2ae80aa822450adb04e98e1202bbd6fdd17e7e36
oreqizer <oreqizer@gmail.com>
2016-05-14 10:52:43 +0200

    config and such

--- reactizer/__init__.py
+++ reactizer/__init__.py
@@ -1,51 +1,22 @@
 import os
-import sqlite3
-from contextlib import closing
 from flask import Flask, jsonify, g, request
+
+from reactizer.database import db_session
 
 
 # initialize app!
 app = Flask(__name__)
 
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'reactizer.db'),
-    DEBUG=True,
-    SECRET_KEY='devops',
-    USERNAME='admin',
-    PASSWORD='reacTizer1337',
-))
-# specifies path to config folder, e.g. 'RIZER_CONFIG=config.py'
+app.config.from_object('reactizer.config')
 app.config.from_envvar('RIZER_CONFIG', silent=True)
 
-
-# database methods
-def connect_db():
-    """Connects to the specific database."""
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-def init_db():
-    """Initializes the database."""
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-# executes before request
-@app.before_request
-def before_request():
-    g.db = connect_db()
+print(app.config)
 
 
 # executes after the request (no shit)
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 @app.route('/api/todos', methods=['GET', 'POST'])
@@ -60,6 +31,3 @@ def show_entries():
         entries = [dict(id=row[0], text=row[1]) for row in cur.fetchall()]
         return jsonify(todos=entries)
 
-
-if __name__ == '__main__':
-    app.run()
---
+++ reactizer/config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+SECRET_KEY = 'potato'
+USERNAME = 'admin'
+PASSWORD = 'reacTizer1337'
