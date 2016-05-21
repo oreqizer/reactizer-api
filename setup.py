dc73c57a3bdb21f44a07b63fe4f884eda8079734
oreqizer <oreqizer@gmail.com>
2016-05-21 16:33:26 +0200

    hopefully fixed gunicorn

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from reactizer import create_app, db
 
 
-if __name__ == "__main__":
+def setup():
     app = create_app()
     with app.app_context():
         db.create_all()
+
+
+def teardown():
+    app = create_app()
+    with app.app_context():
+        db.drop_all()
+
